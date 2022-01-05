762d9cb020db1b9b2c756ef2fed8e9b300378dcc
evanc <evanc@104.231.239.107>
2022-01-05 17:07:33 -0600

    Finishing fields

--- Connect4/src/connectMain.java
+++ Connect4/src/connectMain.java
@@ -1,3 +1,5 @@
+import java.awt.GridLayout;
+
 import javax.swing.*;
 public class connectMain extends JFrame{
 	
@@ -11,6 +13,9 @@ public class connectMain extends JFrame{
 	private static final int ROWS = 6;
 	private static final String PLAYER_1_SYMBOL = "Y";
 	private static final String PLAYER_2_SYMBOL = "R";
+	private static final int EMPTY_SPACE_VALUE = 0;
+	private static final int PLAYER_1_VALUE = 1;
+	private static final int PLAYER_2_VALUE = 2;
 	private boolean run = true;
 	private int[][] board = new int[ROWS][COLUMNS];
 	
@@ -19,5 +24,13 @@ public class connectMain extends JFrame{
 	}
 	public connectMain() {
 		this.setTitle("Connect 4");
+		this.setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setLayout(new GridLayout(ROWS, COLUMNS, 0, 0));
+		this.setResizable(false);
+		this.createContents();
+	}
+	public void createContents() {
+		
 	}
 }
